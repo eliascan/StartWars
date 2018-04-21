315e6d48f8584cc91e9cc4c60e8a0ae0526189d0
Elias Escobar Ramirez <eliascan@yahoo.com>
2018-04-21 10:57:42 -0400

    Initial Commit

---
+++ StartWars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StartWars
+//
+//  Created by Elias Escobar Ramirez on 2018-04-21.
+//  Copyright © 2018 Elias Escobar Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
