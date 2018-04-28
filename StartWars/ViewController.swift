17e303644718ccee142a14194b2a152d72eddabf
ELIAS ESCOBAR RAMIREZ <eliascan@yahoo.com>
2018-04-28 13:19:33 -0400

    HOLA

--- StartWars/ViewController.swift
+++ StartWars/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     let baseURL = "https://swapi.co/api/"
     let getPersonEndPoint = "people/1/"
     
-    
+    // HOLA CLASE
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
