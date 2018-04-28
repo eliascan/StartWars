2a6d47446381a0c6aa9233792a80c6776e0df38e
ELIAS ESCOBAR RAMIREZ <eliascan@yahoo.com>
2018-04-28 12:06:19 -0400

    Successfully made HTTP Requets
    Added Person objetc
    Added Alamofire
    Added ObjectManager

--- StartWars/ViewController.swift
+++ StartWars/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    let baseURL = "https://swapi.co/api/"
+    let getPersonEndPoint = "people/1/"
+    
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var massLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let getPersonURL = URL(string: baseURL + getPersonEndPoint)!
+        Alamofire.request(getPersonURL).responseJSON { response in
+            
+            if response.result.isSuccess {
+                print("This was a success!!!")
+            } else {
+                print("Oh no, there ws an error :(")
+            }
+            
+            let json = response.value
+            print(json as Any)
+            
+            let person = Person("Luke")
+            _ = Person("Alex", height: 190)
+            _ = Person("Elias", height: 185, haireColor: .blond)
+            //print(person)
+            //print(person.getName())
+            self.nameLabel.text = person.name
+            self.massLabel.text = "Mass is \(person.mass)"
+            self.heightLabel.text = person.hairColor.rawValue
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    enum HairColor: String {
+        case light = "Light"
+        case brown = "Brown"
+        case black = "Balck"
+        case blond = "Blond"
     }
-
-
+    
+    
+    struct Person {
+        var name = "Unknowm"
+        var height = 0
+        var mass = 0
+        var hairColor: HairColor = .blond
+        var skinColor = "Unknowm"
+        var eyeColor = "Unknowm"
+        var gender = "Unknowm"
+        var homeworld = "Unknowm"
+        
+        init(_ name: String) {
+            self.name = name
+        }
+        
+        init(_ name: String, height: Int, haireColor : HairColor = .blond) {
+            self.name = name
+            self.height = height
+            self.hairColor = HairColor.blond
+        }
+        
+        func getName() -> String {
+            return self.name
+        }
+    }
 }
-
